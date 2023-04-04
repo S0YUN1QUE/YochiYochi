e7a24e4333612c2a3caee783c2466a82423d5ec9
YunYeongUn <ywyoon03220322@gmail.com>
2023-04-04 15:15:00 +0900

    server update

---
+++ app/Models/GameWord.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gameword extends Model
+{
+    use HasFactory;
+    public $table = "game_words";
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'game_id',
+        'word'
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function games()
+    {
+        return $this->belongsTo('\App\Models\Game', 'game_id', 'id');
+    }
+}
