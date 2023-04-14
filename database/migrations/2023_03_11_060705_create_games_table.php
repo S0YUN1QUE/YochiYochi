83d48869f09874e6961203bfa4959e4ac5f645df
YunYeongUn <ywyoon03220322@gmail.com>
2023-04-14 11:05:36 +0900

    DB&api update

--- database/migrations/2023_03_11_060705_create_games_table.php
+++ database/migrations/2023_03_11_060705_create_games_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('games');
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('name');
