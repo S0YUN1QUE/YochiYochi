00b5b97842164e9c19ad9862d9227ae5986cbba3
YunYeongUn <ywyoon03220322@gmail.com>
2023-04-17 15:28:10 +0900

    api update

--- database/migrations/2023_03_17_060348_create_game_imgs_table.php
+++ database/migrations/2023_03_17_060348_create_game_imgs_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('imgpath');
             $table->unsignedBigInteger('game_id');
+            
             $table->timestamps();
 
             $table->foreign('game_id')->references('id')->on('games');
