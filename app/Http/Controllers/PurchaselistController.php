52e1dd39c268fb4b7097e38d6d428b8594bd4fb0
YunYeongUn <ywyoon03220322@gmail.com>
2023-04-24 15:27:03 +0900

    api update

--- app/Http/Controllers/PurchaselistController.php
+++ app/Http/Controllers/PurchaselistController.php
@@ -4,19 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Item;
-use App\Models\PurchaseList;
+use App\Models\Purchaselist;
 use Illuminate\Support\Facades\Auth;
 
 class PurchaselistController extends Controller
 {
-    public function PurchaseList(){ // 구매목록
-        
-        $lists = PurchaseList::OrderBy('created_at', 'desc')->where('user_id',Auth::guard('api')->user()->id)->get();
-        
-       
-        $returnJson = json_encode($lists);
-       
+    public function index()
+    {
+        $lists = Purchaselist::orderBy('created_at', 'desc')
+            ->where('user_id', Auth::guard('api')->user()->id)
+            ->get();
 
-        return $returnJson;
+        return $lists;
     }
 }
